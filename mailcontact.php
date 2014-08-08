6dec7269e8b192e10c610b010ffe74fba202dfb0
Gabriel Garrett <ggaabe@gmail.com>
2014-08-08 13:07:23 -0400

    Contact form

---
+++ mailcontact.php
@@ -0,0 +1,11 @@
+<?php
+$email = $_POST['email'];
+$name = $_POST['name'];
+$message= $_POST['message'];
+$formcontent="Email: $email \n Name: $name \n Message: $message";
+$recipient = "colonyfrontier@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You! Your contact request has been received by ColonyOne. You will be responded to shortly in regards to your message." . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
