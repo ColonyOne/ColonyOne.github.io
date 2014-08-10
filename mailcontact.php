b9bc44607b64eef111b57c14b85f314ef81a4932
Gabriel Garrett <ggaabe@gmail.com>
2014-08-09 22:58:57 -0400

    php

--- mailcontact.php
+++ mailcontact.php
@@ -8,4 +8,5 @@ $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You! Your contact request has been received by ColonyOne. You will be responded to shortly in regards to your message." . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+header("Location: ".$_SERVER['HTTP_SERVER'].$_POST['back']."");
 ?>
